98bc49d64e07dc0e9abd60352fed69813ccd9e15
Gulshan Singh <gulshan@umich.edu>
2013-07-04 12:10:21 -0700

    Switched from span to mark, styled input, enter to submit, empty string not allowed, cleared old search before doing new one

--- content.js
+++ content.js
@@ -1,17 +1,18 @@
+var marks = new Array();
+
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		var re = new RegExp(request.regexp, "g");
 		var html = document.getElementsByTagName('body')[0];
+		clear();
 		recurse(html, re);
 	});
 
 function recurse(element, regexp)
 {
-	if (element.nodeName == "SPAN" &&
-		element.className.indexOf("_regexp_highlight") != -1) {
+	if (element.nodeName == "MARK") {
 		return;
 	}
-
     if (element.childNodes.length > 0) { 
         for (var i = 0; i < element.childNodes.length; i++) {
 			recurse(element.childNodes[i], regexp);
@@ -23,7 +24,7 @@ function recurse(element, regexp)
 		 * 1. Find all regex matches
 		 * 2. Loop through matches, finding their starting positions
 		 * 3. Add everything between the last match and this match as a text node
-		 * 4. Add the match as a span
+		 * 4. Add the match as a <mark>
 		 * 5. After loop is finished, add remainder as text node
 		 */
 		var str = element.nodeValue;
@@ -39,14 +40,22 @@ function recurse(element, regexp)
 				var before = document.createTextNode(str.substring(pos, index));
 				pos = index + matches[i].length;
 
-				var span = document.createElement('span');
-				span.className = parent.className + " _regexp_highlight";
-				span.appendChild(document.createTextNode(matches[i]));
+				var mark = document.createElement('mark');
+				mark.appendChild(document.createTextNode(matches[i]));
 
 				parent.appendChild(before);
-				parent.appendChild(span);
+				parent.appendChild(mark);
+
+				marks.push(mark);
 			}
 			parent.appendChild(document.createTextNode(str.substring(pos)));
 		}
+	}
+}
+
+function clear() {
+	for (var i = 0; i < marks.length; i++) {
+		var mark = marks[i];
+		mark.parentNode.replaceChild(mark.firstChild, mark);
 	}
 }
--- popup.js
+++ popup.js
@@ -1,9 +1,18 @@
-document.getElementById("search").addEventListener("click", function() {
+document.getElementById("search").addEventListener("click", search);
+document.getElementById("query").addEventListener("keydown", function(event) {
+	if (event.keyCode == 13) {
+		search();
+	}
+});
+
+function search() {
 	chrome.tabs.getSelected(null, function(tab) {
 		var el = document.getElementById("query");
-		chrome.tabs.sendMessage(tab.id, {regexp: el.value});
+		if (el.value != "") {
+			chrome.tabs.sendMessage(tab.id, {regexp: el.value});
+		}
 	});
-});
+}
 
 
 
