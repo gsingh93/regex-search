7d7e75f3ebb9aa0080a6a05fae6fb7c4a9f73ea8
Gulshan Singh <gulshan@umich.edu>
2013-12-21 16:20:21 -0500

    Added update and install pages

--- background.js
+++ background.js
@@ -1,2 +1,10 @@
 var query = "";
 var searching = false;
+
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason == "install") {
+        chrome.tabs.create({ url: "install.html" });
+    } else if (details.reason == "update") {
+        chrome.tabs.create({ url: "update.html" });
+    }
+});
