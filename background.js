0181755f975aa14cc8f8b1d407e743a40879c9c0
Gulshan Singh <gulshan@umich.edu>
2013-12-23 00:23:46 -0500

    Implemented keyboard shortcuts

--- background.js
+++ background.js
@@ -8,3 +8,30 @@ chrome.runtime.onInstalled.addListener(function (details) {
         chrome.tabs.create({ url: "update.html" });
     }
 });
+
+var lastCalled = 0;
+chrome.commands.onCommand.addListener(function(command) {
+    // The time hack is to get around this function being called twice when the
+    // popup is open
+    var d = new Date();
+    if (searching && d.getTime() - lastCalled > 50) {
+        if (command == "next") {
+            sendCommand("next");
+        } else if (command == "prev") {
+            sendCommand("prev");
+        }
+        lastCalled = d.getTime();
+    }
+});
+
+function sendCommand(commandName, responseHandler) {
+    (function (commandName, responseHandler) {
+        chrome.tabs.getSelected(null, function(tab) {
+            if (responseHandler == undefined) {
+                responseHandler = null;
+            }
+            chrome.tabs.sendMessage(tab.id, {command: commandName},
+                                    responseHandler);
+        });
+    })(commandName, responseHandler);
+}
