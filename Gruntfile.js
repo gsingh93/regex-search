9cba3e3fe7a95f72d4043e1434447b9a55bfb8cc
ComFreek <comfreek@outlook.com>
2014-01-06 14:11:01 +0100

    Improved Grunt build system

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,52 @@ module.exports = function(grunt) {
 	"use strict";
 	
 	var bgSrc = ["src/background/*.ts"];
+	var popupSrc = ["src/popup/*.ts"];
 	
 	grunt.initConfig({
 		ts: {
-			devBg: {
+			bg: {
 				src: bgSrc,
-				out: "build/dev/background.js",
+				out: "<%= buildDir %>/background/background.js",
 			},
-			relaseBg: {
-				src: bgSrc,
-				out: "build/release/background.js"
+			popup: {
+				src: popupSrc,
+				out: "<%= buildDir %>/popup/popup.js"
+			}
+		},
+		copy: {
+			all: {
+				files: [
+					{
+						expand: true,
+						src: ["manifest.json", "content/*", "pages/*", "popup/popup.html"],
+						cwd: "src/",
+						dest: "<%= buildDir %>/"
+					},
+					{
+						expand: true,
+						src: "images/*",
+						dest: "<%= buildDir %>/"
+					}
+				]
 			}
 		}
 	});
 	
 	grunt.loadNpmTasks("grunt-ts");
-	grunt.registerTask("default", ["ts:devBg"]);
+	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.registerTask("default", ["dev"]);
+	
+	function runAllTasks() {
+		grunt.task.run(["ts:bg", "ts:popup", "copy:all"]);
+	}
+	
+	grunt.registerTask('dev', 'Build dev', function() {
+		grunt.config("buildDir", "build/dev");
+		runAllTasks();
+	});
+	grunt.registerTask('release', 'Build dev', function() {
+		grunt.config("buildDir", "build/release");
+		runAllTasks();
+	});
 };
